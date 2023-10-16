176e3e9c4c1d3169c220901e64a6f313f190d0f4
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-10-16 10:25:10 -0700

    added localStorage

--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -14,10 +14,25 @@ const Todo = () => {
   const [defaultValues] = useState({
     difficulty: 4,
   });
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(() => {
+    // getting stored value
+    const savedList = localStorage.getItem("list");
+    const initialValue = JSON.parse(savedList);
+    return initialValue || [];
+  });
   const [incomplete, setIncomplete] = useState([]);
-  const [showFullList, setShowFullList] = useState(false);
-  const [showComplete, setShowComplete] = useState(false);
+  const [showFullList, setShowFullList] = useState(() => {
+    // getting stored value
+    const savedShowFullList = localStorage.getItem("showFullList");
+    const initialValue = JSON.parse(savedShowFullList);
+    return initialValue || false;
+  });
+  const [showComplete, setShowComplete] = useState(() => {
+    // getting stored value
+    const savedShowComplete = localStorage.getItem("showComplete");
+    const initialValue = JSON.parse(savedShowComplete);
+    return initialValue || false;
+  });
   const [showAddJobOrSettings, setShowAddJobOrSettings] = useState('addJob');
   const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
 
@@ -47,17 +62,23 @@ const Todo = () => {
   }
 
   function toggleList() {
-    setShowFullList(!showFullList);
+    setShowFullList((prev) => {
+      const newShowFullList = !prev
+      localStorage.setItem("showFullList", JSON.stringify(newShowFullList));
+      return newShowFullList;
+    });
   }
 
   useEffect(() => {
     let incompleteCount = list.filter(item => !item.complete).length;
     setIncomplete(incompleteCount);
     document.title = `To Do List: ${incomplete}`;
+    localStorage.setItem("list", JSON.stringify(list));
     // linter will want 'incomplete' added to dependency array unnecessarily. 
     // disable code used to avoid linter warning 
-    // eslint-disable-next-line react-hooks/exhaustive-deps 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [list]);  
+
 
   return (
     <Context.Provider value={{setShowComplete, toggleList, setShowAddJobOrSettings, incomplete, list, toggleComplete, handleChange, handleSubmit, defaultValues, showComplete, showFullList}}>
