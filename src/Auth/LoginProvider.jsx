6798f7274a350cee500541e9dd3d36dc2734b11a
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-10-23 20:27:51 -0700

    yay

--- src/Auth/LoginProvider.jsx
+++ src/Auth/LoginProvider.jsx
@@ -1,32 +1,40 @@
 import React, { useCallback, useEffect, useState } from "react";
 import cookie from "react-cookies";
 import jwt_decode from "jwt-decode";
+import axios from "axios";
 
-const testUsers = {
-  Administrator: {
-    password: "admin",
-    name: "Administrator",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW5pc3RyYXRvciIsInJvbGUiOiJhZG1pbiIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJywncmVhZCcsJ3VwZGF0ZScsJ2RlbGV0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.pAZXAlTmC8fPELk2xHEaP1mUhR8egg9TH5rCyqZhZkQ",
-  },
-  Editor: {
-    password: "editor",
-    name: "Editor",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRWRpdG9yIiwicm9sZSI6ImVkaXRvciIsImNhcGFiaWxpdGllcyI6IlsncmVhZCcsJ3VwZGF0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.3aDn3e2pf_J_1rZig8wj9RiT47Ae2Lw-AM-Nw4Tmy_s",
-  },
-  Writer: {
-    password: "writer",
-    name: "Writer",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVyIiwicm9sZSI6IndyaXRlciIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.dmKh8m18mgQCCJp2xoh73HSOWprdwID32hZsXogLZ68",
-  },
-  User: {
-    password: "user",
-    name: "User",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVXNlciIsInJvbGUiOiJ1c2VyIiwiY2FwYWJpbGl0aWVzIjoiWydyZWFkJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.WXYvIKLdPz_Mm0XDYSOJo298ftuBqqjTzbRvCpxa9Go",
-  },
+// const testUsers = {
+//   Administrator: {
+//     password: "admin",
+//     name: "Administrator",
+//     token:
+//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW5pc3RyYXRvciIsInJvbGUiOiJhZG1pbiIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJywncmVhZCcsJ3VwZGF0ZScsJ2RlbGV0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.pAZXAlTmC8fPELk2xHEaP1mUhR8egg9TH5rCyqZhZkQ",
+//   },
+//   Editor: {
+//     password: "editor",
+//     name: "Editor",
+//     token:
+//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRWRpdG9yIiwicm9sZSI6ImVkaXRvciIsImNhcGFiaWxpdGllcyI6IlsncmVhZCcsJ3VwZGF0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.3aDn3e2pf_J_1rZig8wj9RiT47Ae2Lw-AM-Nw4Tmy_s",
+//   },
+//   Writer: {
+//     password: "writer",
+//     name: "Writer",
+//     token:
+//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVyIiwicm9sZSI6IndyaXRlciIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.dmKh8m18mgQCCJp2xoh73HSOWprdwID32hZsXogLZ68",
+//   },
+//   User: {
+//     password: "user",
+//     name: "User",
+//     token:
+//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVXNlciIsInJvbGUiOiJ1c2VyIiwiY2FwYWJpbGl0aWVzIjoiWydyZWFkJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.WXYvIKLdPz_Mm0XDYSOJo298ftuBqqjTzbRvCpxa9Go",
+//   },
+// };
+
+const capabilities = {
+  Administrator: ["create", "update", "delete"],
+  Editor: ["create", "update"],
+  Writer: ["create"],
+  User: [],
 };
 
 export const LoginContext = React.createContext();
@@ -45,6 +53,7 @@ const LoginProvider = (props) => {
   const validateToken = useCallback((token) => {
     try {
       let validUser = jwt_decode(token);
+      validUser.capabilities = capabilities[validUser.username];
       setLoginState(true, token, validUser);
     } catch (e) {
       setLoginState(false, null, {}, e);
@@ -63,9 +72,17 @@ const LoginProvider = (props) => {
 
   const login = async (username, password) => {
     let { loggedIn, token, user } = state;
-    let auth = testUsers[username];
+    const config = {
+      baseURL: "https://lab34server.onrender.com",
+      url: "/signin",
+      method: "post",
+      auth: { username, password },
+    };
 
-    if (auth && auth.password === password) {
+    const response = await axios(config);
+    const auth = response.data.user;
+    console.log(auth);
+    if (auth && auth.username === username) {
       try {
         validateToken(auth.token);
       } catch (e) {
